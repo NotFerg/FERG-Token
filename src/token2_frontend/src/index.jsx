648a942457c32b07046e4cc0a637be0c5c7f9371
Fergus <fergus.ampuan@gmail.com>
2024-07-16 17:13:38 +0800

    Added authentication

--- src/token2_frontend/src/index.jsx
+++ src/token2_frontend/src/index.jsx
@@ -1,13 +1,30 @@
 import ReactDOM from 'react-dom'
 import React from 'react'
 import App from "./components/App";
+import {AuthClient} from "@dfinity/auth-client"
 
 const init = async () => { 
-  ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>,
-  );
+  const authClient = await AuthClient.create();
+
+  if(await authClient.isAuthenticated()){
+    handleAuthenticated(authClient);
+  }else{
+    await authClient.login({
+      identityProvider: "https://identity.ic0.app/#authorize",
+      onSuccess: () =>{
+        handleAuthenticated(authClient)
+      }
+    })
+  }
+
+  async function handleAuthenticated(authClient){
+    ReactDOM.createRoot(document.getElementById('root')).render(
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>,
+    );
+    
+  }
 }
 
 init();
