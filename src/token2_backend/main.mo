3c4295abcd2baac525a7f61eee30f9ff4bc6f7f3
Fergus <fergus.ampuan@gmail.com>
2024-07-16 11:06:09 +0800

    Added claim token functionality

--- src/token2_backend/main.mo
+++ src/token2_backend/main.mo
@@ -23,4 +23,16 @@ actor Token{
     public query func getSymbol() : async Text {
         return symbol;
     };
+
+    public shared (msg) func payOut() : async Text {
+        // Debug.print(debug_show(msg.caller));
+        if(balances.get(msg.caller) == null){
+             let amount = 10000;
+             balances.put(msg.caller, amount);
+            return "Success";
+        } else {
+            return "You have already claimed your reward";
+        }
+       
+    }
 };
--- src/token2_frontend/src/components/Faucet.jsx
+++ src/token2_frontend/src/components/Faucet.jsx
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
+import { token2_backend } from "../../../declarations/token2_backend";
 
 function Faucet() {
+  const [isDisabled, setDisabled] = useState(false);
+  const [buttonText,setButtonText] = useState("Gimme gimme");
 
   async function handleClick(event) {
-
+    setDisabled(true);
+    const result = await token2_backend.payOut();
+    setButtonText(result);
+    //setDisabled(false);
   }
 
   return (
@@ -14,10 +20,13 @@ function Faucet() {
         </span>
         Faucet
       </h2>
-      <label>Get your free DAngela tokens here! Claim 10,000 DANG coins to your account.</label>
+      <label>
+        Get your free FERG tokens here! Claim 10,000 FERG tokens to your
+        account.
+      </label>
       <p className="trade-buttons">
-        <button id="btn-payout" onClick={handleClick}>
-          Gimme gimme
+        <button id="btn-payout" onClick={handleClick} disabled={isDisabled}>
+          {buttonText}
         </button>
       </p>
     </div>
